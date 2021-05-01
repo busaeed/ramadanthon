3311fc0b4fe79cf60968b8560a160b077ca5f4d5
S-MW <72529306+S-MW@users.noreply.github.com>
2021-05-01 05:14:59 +0300

    add form & show it

--- app/Http/Controllers/movePageController.php
+++ app/Http/Controllers/movePageController.php
@@ -4,13 +4,23 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Trip;
+use Illuminate\Support\Facades\Auth;
+
 class movePageController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+        
+    }
     //
 
     public function GoToOrgControlPanel(){
 
-        return view('orgControlPanel');
+        $Trips = Trip::all();
+        return view('orgControlPanel',compact('Trips'));
     }
 
     public function GoToVolunteerNew(){
@@ -22,6 +32,21 @@ class movePageController extends Controller
 
         return view('volunteerPast');
     }
+
+    public function create(){
+
+        return view('create');
+    }
+
+    public function store(Request $request){
+        
+        $user = Auth::user();
+        $user->trips()->create($request->all());
+
+        //$Trips = Trip::create($request->all());
+        return view('orgControlPanel');
+    }
+
 
 
 }
---
+++ resources/views/create.blade.php
@@ -0,0 +1,54 @@
+@extends('layouts.app')
+@section('content')
+<h1>Add new Trip</h1>
+<form action="{{ route('store') }}" method="post"  enctype="multipart/form-data">
+    @csrf 
+        
+            <div class="form-group">
+            <label for="name"><span style="color:red;">* </span>Trip Name</label>
+            <input type="text" name="name" id="name" placeholder="Name" class="form-control" required>
+            </div>
+            <!--             
+            <div class="form-group">
+                <label for="city"><span style="color:red;">* </span>City</label>
+                <select name="city" id="city" class="form-control">
+                    <option value="Riyadh">Riyadh</option>
+                    <option value="Jeddah">Jeddah</option>
+                    <option value="Dammam">Dammam</option>
+                </select>
+            </div> 
+            -->
+
+            <div class="form-group">
+                <label for="description"><span style="color:red;">* </span>Description</label>
+                <input type="text" name="description" id="description" placeholder="description" class="form-control" required>
+            </div>
+
+            <div class="form-group">
+                <label for="gender"><span style="color:red;">* </span>Gender</label>
+                <select name="gender" id="gender" class="form-control">
+                    <option value="M">Male</option>
+                    <option value="F">Female</option>
+                    <option value="MF">Male & Female</option>
+                </select>
+            </div>
+
+            <div class="form-group">
+                <label for="seats"><span style="color:red;">* </span>Seats</label>
+                <input type="number" name="seats" id="seats" placeholder="seats" class="form-control" required>
+            </div>
+
+            <div class="form-group">
+                <label for="scheduled_at"><span style="color:red;">* </span>scheduled_at</label>
+                <input type="datetime-local" name="scheduled_at" id="scheduled_at"  class="form-control" required>
+            </div>
+
+
+            <!-- Submit And Reset Button   -->
+            <input class="btn btn-dark" type="submit" >
+            <input class="btn btn-dark" type="reset">
+
+           
+        </form>
+
+@endsection
