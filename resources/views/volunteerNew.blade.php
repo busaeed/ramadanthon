7af763b196fc02813c758c830a670f7a7e992575
Mohammed Bu Saeed <bosaeed2011@gmail.com>
2021-05-01 08:18:21 +0300

    blade bug was fixed

--- resources/views/volunteerNew.blade.php
+++ resources/views/volunteerNew.blade.php
@@ -1,3 +1,6 @@
+@extends('layouts.app')
+@section('content')
+
 <!DOCTYPE html>
 <!DOCTYPE html>
 <html lang="en">
@@ -8,7 +11,7 @@
     <title>orgControlPanel page</title>
 </head>
 <body>
-    <h1>volunteerNew </h1>
+    <h1>الفرص التطوعية المتاحة</h1>
     @foreach ($trips as $trip)
         Trip Id: {{$trip->id}}<br>
         Trip Name: {{$trip->name}}<br>
@@ -17,8 +20,10 @@
         Trip Date: {{$trip->scheduled_at}}<br>
         Trip Seats: {{$trip->seats}}<br>
         Trip Available Seats: {{$trip->available_seats}}<br>
-        <a href="trip/{{$trip->id}}">Click here for more details</a><br>
+        <a style="font-weight: bold;" href="trip/{{$trip->id}}">Click here for more details</a><br>
         <br>
     @endforeach
 </body>
 </html>
+
+@endsection
