4c769cb02c3d9d0b761ad12d7b6429a3a7cd31df
S-MW <72529306+S-MW@users.noreply.github.com>
2021-05-01 06:26:09 +0300

    add Past trips

--- resources/views/volunteerPast.blade.php
+++ resources/views/volunteerPast.blade.php
@@ -8,6 +8,34 @@
     <title>orgControlPanel page</title>
 </head>
 <body>
-    <h1>volunteerNew</h1>
+    <h1>volunteerPast</h1>
+
+    <table class="table table-striped">
+            <thead>
+            <tr>
+                <th >ID</th>
+                <th >Name</th>
+                <th >City</th>
+                <th >Description</th>
+                <th >Seats</th>
+                <th >Gender</th>
+                <th >Scheduled_at</th>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach($Trips as $Trip)
+                <tr>
+                    <td>{{$Trip->id}}</td>
+                    <td>{{$Trip->name}}</td>
+                    <td>{{$Trip->city}}</td>
+                    <td>{{$Trip->description}}</td>
+                    <td>{{$Trip->seats}}</td>
+                    <td>{{$Trip->gender}}</td>
+                    <td>{{$Trip->scheduled_at}}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+
 </body>
 </html>
